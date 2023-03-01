86f4c79624b327e5491453d0e9ad113e3748f375
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-02 08:48:45 +0900

    talks: fix models repr show sentence

--- backend/tests/test_models/test_talks.py
+++ backend/tests/test_models/test_talks.py
@@ -5,4 +5,4 @@ from api.models.talks import Talk as TalkModel
 def test_talk_model_repr():
     sentence = SentenceModel(id=1, sentence="hoge", translation="hige", counter=0)
     talk = TalkModel(id=1, order_id=1, sentence=sentence)
-    assert str(talk) == f"<Talk (id, order, sentence) = (1, 1, 1)>"
+    assert str(talk) == f"<Talk (id, order, sentence) = (1, 1, hoge)>"
