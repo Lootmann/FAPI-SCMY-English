4ebca9b977b81da3f31db3ff12bd443d36036adc
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-01 21:13:04 +0900

    models: SentenceModel - set default value to conter = 0

--- backend/api/models/sentences.py
+++ backend/api/models/sentences.py
@@ -10,7 +10,7 @@ class Sentence(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     sentence: Mapped[str]
     translation: Mapped[str]
-    counter: Mapped[int]
+    counter: Mapped[int] = mapped_column(default=0)
 
     def __repr__(self) -> str:
         return f"<Sentence (id, sent, trans, counter) = ({self.id}, {self.sentence}, {self.translation}, {self.counter})>"
