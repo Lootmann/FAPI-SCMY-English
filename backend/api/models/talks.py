480ad49cae95d4c941c06c20e9838c412c02fef9
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-02 07:10:11 +0900

    talks: models repr test

--- backend/api/models/talks.py
+++ backend/api/models/talks.py
@@ -10,12 +10,12 @@ class Talk(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     order_id: Mapped[int]
 
-    # Sentence
+    # Sentence : Talk = 1 : 1
     sentence_id: Mapped[int] = ForeignKey("sentences.id")
     sentence: Mapped["Sentence"] = relationship("Sentence", uselist=False)
 
     def __repr__(self) -> str:
         return (
-            "<Talk (id, order, sentence_id)"
-            + f" = ({self.id}, {self.order_id}, {self.sentence_id})>"
+            "<Talk (id, order, sentence)"
+            + f" = ({self.id}, {self.order_id}, {self.sentence.id})>"
         )
---
+++ backend/tests/test_models/test_talks.py
@@ -0,0 +1,8 @@
+from api.models.sentences import Sentence as SentenceModel
+from api.models.talks import Talk as TalkModel
+
+
+def test_talk_model_repr():
+    sentence = SentenceModel(id=1, sentence="hoge", translation="hige", counter=0)
+    talk = TalkModel(id=1, order_id=1, sentence=sentence)
+    assert str(talk) == f"<Talk (id, order, sentence) = (1, 1, 1)>"
