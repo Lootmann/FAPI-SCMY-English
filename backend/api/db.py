37ec02677d533c849aed42c6a54f1d1ff790a562
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-01 21:10:45 +0900

    backend: db - db setting for asycn session

--- backend/api/db.py
+++ backend/api/db.py
@@ -1,17 +1,22 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from api.settings import Settings
 
 setting = Settings()
 
+async_engine = create_async_engine(setting.async_db_url, echo=True)
+async_session = sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    autoflush=False,
+    autocommit=False,
+)
 
-engine = create_engine(setting.async_db_url, echo=True)
-session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
 
-def get_db():
-    with session() as s:
-        yield s
+async def get_db():
+    async with async_session() as session:
+        yield session
