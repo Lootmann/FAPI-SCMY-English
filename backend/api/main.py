6813c239203bfe94f23ff22206eb3aab298337d3
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-02 11:43:18 +0900

    routers: add talks router

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from api.routers.histories import router as history_router
 from api.routers.sentences import router as sentence_router
+from api.routers.talks import router as talk_router
 from api.routers.words import router as word_router
 
 app = FastAPI()
@@ -9,3 +10,4 @@ app = FastAPI()
 app.include_router(sentence_router)
 app.include_router(word_router)
 app.include_router(history_router)
+app.include_router(talk_router)
