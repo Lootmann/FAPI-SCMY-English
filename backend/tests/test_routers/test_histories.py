d3d9bf4829d2e1e47bee65e0a75349520ffc7424
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-03 07:08:10 +0900

    histories: fix async to db connection D:

--- backend/tests/test_routers/test_histories.py
+++ backend/tests/test_routers/test_histories.py
@@ -1,65 +1,60 @@
-import pytest
 from fastapi import status
 
-from tests.init_async_client import async_client as client
+from tests.init_client import client
 
 
-@pytest.mark.asyncio
 class TestGetAllHistoires:
-    async def test_get_all_histories(self, client):
-        resp = await client.get("/histories")
+    def test_get_all_histories(self, client):
+        resp = client.get("/histories")
         assert resp.status_code == status.HTTP_200_OK
         assert resp.json() == []
 
 
-@pytest.mark.asyncio
 class TestGetHistory:
-    async def test_get_history(self, client):
-        resp = await client.post("/histories")
+    def test_get_history(self, client):
+        resp = client.post("/histories")
         assert resp.status_code == status.HTTP_201_CREATED
 
         history_id = resp.json()["id"]
-        resp = await client.get(f"/histories/{history_id}")
+        resp = client.get(f"/histories/{history_id}")
         assert resp.status_code == status.HTTP_200_OK
 
-    async def test_get_history_wrong_id(self, client):
-        resp = await client.get("/histories/1")
+    def test_get_history_wrong_id(self, client):
+        resp = client.get("/histories/1")
         assert resp.status_code == status.HTTP_404_NOT_FOUND
 
 
-@pytest.mark.asyncio
 class TestPostHistory:
-    async def test_post_history(self, client):
-        resp = await client.get("/histories")
+    def test_post_history(self, client):
+        resp = client.get("/histories")
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.json()) == 0
 
-        resp = await client.post("/histories")
+        resp = client.post("/histories")
         assert resp.status_code == status.HTTP_201_CREATED
         assert resp.json()["id"] == 1
 
-        resp = await client.get("/histories")
+        resp = client.get("/histories")
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.json()) == 1
 
 
-@pytest.mark.asyncio
 class TestDeleteHistory:
-    async def test_delete_history(self, client):
-        resp = await client.post("/histories")
+    def test_delete_history(self, client):
+        resp = client.post("/histories")
         assert resp.status_code == status.HTTP_201_CREATED
 
-        resp = await client.get("/histories")
+        resp = client.get("/histories")
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.json()) == 1
 
         history_id = resp.json()[0]["id"]
-        resp = await client.delete(f"/histories/{history_id}")
+        resp = client.delete(f"/histories/{history_id}")
 
-        resp = await client.get("/histories")
+        resp = client.get("/histories")
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.json()) == 0
 
-    async def test_delete_history_with_wrong_id(self, client):
-        resp = await client.delete("/histories/9837124")
+    def test_delete_history_with_wrong_id(self, client):
+        resp = client.delete("/histories/9837124")
         assert resp.status_code == status.HTTP_404_NOT_FOUND
