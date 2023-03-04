ef37246f36838c411f881fc493f8335ec25a0319
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-05 07:51:25 +0900

    frontend: sentence create, update, detail

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
 import Header from "./header";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="text-xl">
+    <div className="text-xl h-screen flex flex-col">
       <Header />
 
-      <div className="p-4">
-        <Outlet />
+      <div className="flex justify-center h-[calc(100vh-4rem)]">
+        <div className="h-full w-3/4">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
